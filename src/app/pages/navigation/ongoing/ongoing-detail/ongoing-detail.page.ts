4e1161d1ceb2bf0454247fb318f0cc97b36706e3
trantrongnhan <ttnhan@brickmate.kr>
2020-08-31 16:04:48 +0700

    Missing loader service

--- src/app/pages/navigation/ongoing/ongoing-detail/ongoing-detail.page.ts
+++ src/app/pages/navigation/ongoing/ongoing-detail/ongoing-detail.page.ts
@@ -30,13 +30,13 @@ export class OngoingDetailPage implements OnInit {
 	) {
 		this.id = this.route.snapshot.paramMap.get('id'); //get id parameter
 
-		translate.addLangs(['en', 'kr']);
+		this.translate.addLangs(['en', 'kr']);
 
 		// this language will be used as a fallback when a translation isn't found in the current language
-		translate.setDefaultLang('kr');
+		this.translate.setDefaultLang('kr');
 
 		// the lang to use, if the lang isn't available, it will use the current loader to get them
-		translate.use('kr');
+		this.translate.use('kr');
 	}
 
 	ngOnInit() {
